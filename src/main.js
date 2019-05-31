0762884f057a24b980f06f3fef4a30fc5d73fa04
Katlin <katlin.nbanderson@gmail.com>
2019-05-31 15:52:18 -0700

    add beginning og frontend logic

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-// import { Age } from './age-calc';
+import { Age } from './age-calc';
 import './styles.css';
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+  $('#age-form').submit(function(event) {
+    event.preventDefault();
+    let birthDay = $('#birthDay').val();
+    console.log(birthDay);
+    let userDate = new Date(birthDay);
+    let health = $('#health').val();
+    let intelligence = $('#intelligence').val();
+    let userAge = new Age(userDate, health, intelligence);
+    console.log(userAge.earthAge);
+  })
+})
