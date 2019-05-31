bdd4b66f4dccbe9b736c6f33a4803551fa2eb0b5
Katlin <katlin.nbanderson@gmail.com>
2019-05-31 16:16:07 -0700

    connect the hatml form to the ui

--- src/main.js
+++ src/main.js
@@ -7,12 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 $(document).ready(function() {
   $('#age-form').submit(function(event) {
     event.preventDefault();
-    let birthDay = $('#birthDay').val();
-    console.log(birthDay);
-    let userDate = new Date(birthDay);
-    let health = $('#health').val();
-    let intelligence = $('#intelligence').val();
-    let userAge = new Age(userDate, health, intelligence);
-    console.log(userAge.earthAge);
+    let userDate = $("#birthDate").val();
+    let date = new Date(userDate);
+    let health = $('input:radio[name=health]:checked').val();
+    let intelligence = $('input:radio[name=intelligence]:checked').val();
+    let userAge = new Age(date, health, intelligence);
+    console.log(userAge);
+
+
   })
 })
