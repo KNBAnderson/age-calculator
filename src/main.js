910b823fb15f1c3b13e5a78e50a51ae534d7c743
Katlin <katlin.nbanderson@gmail.com>
2019-05-31 16:32:48 -0700

    add solution display to ui

--- src/main.js
+++ src/main.js
@@ -12,8 +12,20 @@ $(document).ready(function() {
     let health = $('input:radio[name=health]:checked').val();
     let intelligence = $('input:radio[name=intelligence]:checked').val();
     let userAge = new Age(date, health, intelligence);
-    console.log(userAge);
 
-
+    $('#solution').append('<h3>Your age</h3>');
+    $('#solution').append(`<li>On Earth, you are ${userAge.earthAge} years old.</li>`);
+    $('#solution').append(`<li>On Mercury, you are ${userAge.mercuryAge} years old.</li>`);
+    $('#solution').append(`<li>On Venus, you are ${userAge.venusAge} years old.</li>`);
+    $('#solution').append(`<li>On Mars, you are ${userAge.marsAge} years old.</li>`);
+    $('#solution').append(`<li>On Jupiter, you are ${userAge.jupiterAge}</li> years old.</li>`);
+    $('#solution').append(`<li>Based on you answeres, your life expectancy is ${userAge.lifeExpectancy} years old.</li>`);
+    if (userAge.lifeExpectancy > userAge.earthAge) {
+      $('#solution').append(`<li>You have ${userAge.lifeExpectancy - userAge.earthAge} years left to live.</li>`);
+    } else if (userAge.lifeExpectancy = userAge.earthAge) {
+      $('#solution').append(`<li>Your end is very near.</li>`);
+    } else if (userAge.lifeExpectancy < userAge.earthAge) {
+      $('#solution').append(`<li>You have lived ${userAge.earthAge - userAge.lifeExpectancy} years passed your life expectancy.</li>`);
+    }
   })
 })
